69b3019f4752d2748fadde4a06883302a5b3ed2c
Clark Whitehead <clark12341@gmail.com>
2021-07-24 21:29:33 -1000

    test

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WRRC Data Portal
+          WRRC Data Portals Site
         </p>
       </header>
     </div>
