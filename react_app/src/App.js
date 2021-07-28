afd9654ea8cbe76abc037db2c9746a15ce964f46
Clark Whitehead <clark12341@gmail.com>
2021-07-27 21:39:45 -1000

    getting Dropfile to work

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Header } from './components/Header'; 
+import { ImgDrop } from './components/ImgDrop';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ImgDrop />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ react_app/src/components/ImgDrop.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dropzone from 'react-dropzone';
+
+export class ImgDrop extends React.Component {	
+    <Dropzone>
+    {dropzoneProps => {	  
+    return (
+    	<div>
+	    <h1>Drop your files here</h1>
+	</div>
+    );
+  }}
+  </Dropzone>;
+  }
