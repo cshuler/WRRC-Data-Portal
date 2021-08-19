ae726a775620ae292d3e7ef419cc605961c21a16
Clark Whitehead <clark12341@gmail.com>
2021-08-19 11:48:38 +0000

    finished except for console loggin instead of posting to page

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -15,27 +15,47 @@ function App() {
 
 */
 
+/*
 	const data = require('./data.json');
 
 	const title = data['title'];
 	const author = data['authors'];
 	const keywords = data['keywords'];
+*/
 
-    const crazy = () => {
-    const xhr = new XMLHttpRequest();
-    const formData = new FormData();
+	function getJson(){
+
+		fetch('http://clarkwhitehead.com/data.json')
+		  	.then(response => response.json())
+		  	.then(data => console.log(data));
+
+	}
+
+    async function handleClick(){
+    	const xhr = new XMLHttpRequest();
+    	const formData = new FormData();
 
 
-    const inpFile = document.getElementById("inpFile");
+    	const inpFile = document.getElementById("inpFile");
 
-	   for (const file of inpFile.files){
-		formData.append("myFiles[]", file);
-	   }
+	    for (const file of inpFile.files){
+			formData.append("myFiles[]", file);
+	   	}
 
-	   xhr.open("post", "http://clarkwhitehead.com/upload.php");
-	   xhr.send(formData);
+	   	xhr.open("post", "http://clarkwhitehead.com/upload.php");
+
+	   	xhr.send(formData);
+ 		xhr.onreadystatechange = function(){
+			if (xhr.readyState === 4) {
+
+				getJson();
+
+			}
+		};
+	
     }
 
+	
 
   return (
     <div className="App">
@@ -44,11 +64,7 @@ function App() {
 
 	
 	  <input type="file" id="inpFile" multiple />
-	  <button onClick={crazy} id="btnUpload">Upload Files</button>
-	  
-		<h1>Title: {title}</h1>
-		<h1>Author: {author}</h1>
-		<h1>Keywords: {keywords}</h1>
+	  <button onClick={handleClick} id="btnUpload">Upload Files</button>
 
 	</header>
     </div>
